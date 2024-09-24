fd36a2f5c5980f1eb5737847a55b5d42bfa7334e
Иван Кулаков <vn8007@gmail.com>
2024-09-24 18:39:26 +0300

    swipe

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ try{
     const bestContentItems = document.getElementsByClassName('best_content')
     let timerId = setInterval(() => stepRight(), 3000);
 
+
     const idForItems = () => {
         for (let i = 0; i < bestTitleItem.length; i++){
             bestTitleItem[i].id = 'titleId' + i;
@@ -62,6 +63,108 @@ catch (e) {
     console.log(e)
 }
 // end best block
+
+
+// start best block mobile
+try{
+    const bestItemsMob = document.getElementsByClassName('best_mobile');
+    const placeForNembers = document.getElementById('numbersOfBstItems');
+    const rightButton = document.getElementById('svgRight');
+    const leftButton = document.getElementById('svgLeft');
+    const swipeBestMobile = document.getElementById('best_mobile');
+
+    let timerIdMob = setInterval(() => stepRightMobTimer(), 3000);
+
+
+    const idForItemsMob = () => {
+        for (let i = 0; i < bestItemsMob.length; i++) {
+            bestItemsMob[i].id = 'bestItemMobileId' + i;
+            bestItemsMob[i].classList.add('invisible');
+            bestItemsMob[0].classList.remove('invisible');
+        }
+            // const counter = bestItemsMob.length;
+            // const arrCounter = Array.from({length: counter}, (_, index) => index + 1);
+            // console.log(arrCounter);
+            // arrCounter.forEach(i => placeForNembers.innerHTML += ` <div>
+            //     <p id="currentBox${i - 1}">${i}</p>
+            // </div>`)
+            //
+    }
+    const isActive = () => {
+        for(let i = 0; i < bestItemsMob.length; i++){
+            if(!bestItemsMob[i].classList.contains('invisible')){
+                return Number(bestItemsMob[i].id.slice(-1))
+            }
+        }
+    }
+    const stepRightMobTimer = () =>{
+        let next;
+        if(+isActive() === (bestItemsMob.length-1)){
+            next = 0;
+        }
+        else next = +isActive() +1;
+        for (let i = 0; i < bestItemsMob.length; i++){
+            bestItemsMob[i].classList.add('invisible');
+            bestItemsMob[next].classList.remove('invisible')
+        }
+    }
+    const stepLeftMob = () =>{
+        clearInterval(timerIdMob);
+        let next;
+        if(+isActive() === 0){
+            next = (bestItemsMob.length-1);
+        }
+        else next = +isActive() -1;
+        for (let i = 0; i < bestItemsMob.length; i++){
+            bestItemsMob[i].classList.add('invisible');
+            bestItemsMob[next].classList.remove('invisible')
+        }
+    }
+    const stepRightMob = () =>{
+        clearInterval(timerIdMob);
+        stepRightMobTimer();
+    }
+    let x1 = null;
+    let y1 = null;
+
+    const handleTouchStart = (event) => {
+        clearInterval(timerIdMob);
+        const firstTouch = event.touches[0];
+        x1 = firstTouch.clientX;
+        y1 = firstTouch.clientY;
+    }
+    const handleToucheMove = (event) =>{
+        if(!x1 || !y1){
+            return false;
+        }
+        let x2 = event.touches[0].clientX
+        let y2 = event.touches[0].clientY
+        let xDiff = x2 - x1;
+        let yDiff = y2 - y1;
+        if(Math.abs(xDiff) < Math.abs(yDiff)){
+            return false;
+        }
+        if (xDiff > 0){
+            stepRightMobTimer();
+        }
+        if(xDiff < 0){
+            stepLeftMob();
+        }
+        x1 = null;
+        y1 = null;
+    }
+
+    swipeBestMobile.addEventListener('touchstart', handleTouchStart, false);
+    swipeBestMobile.addEventListener('touchmove', handleToucheMove, false);
+    rightButton.addEventListener("click", stepRightMob);
+    leftButton.addEventListener("click",stepLeftMob);
+    idForItemsMob();
+}
+catch (e) {
+    console.log(e)
+}
+// end best block mobile
+
 
 //btn-up block start
 try{
